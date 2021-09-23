698bcbd86e00fdb040b0af683f73072cda67fe4c
unnursoley <63641610+unnursoley@users.noreply.github.com>
2021-09-23 21:58:37 +0000

    Update Point.java

--- Distr/Point.java
+++ Distr/Point.java
@@ -34,20 +34,31 @@ public class Point implements Comparable<Point> {
         StdDraw.line(this.x, this.y, that.x, that.y);
     }
 
-    // slope between this point and that point
+   // slope between this point and that point
     public double slopeTo(Point that) {
-        // TODO: Implement this
-        return 0;
-    }
+        if (that.x - this.x == 0)
+            return 0;
+        else if (that.y - this.y == 0)
+            return 0;
+        else if (that.x - this.x == 0 && that.y - this.y == 0)
+            return 0;
+        else
+            return (that.y - this.y) / (that.x - this.x);
+    } // Get ekki sett inn positive inf of negative inf, finn út úr því seinna
 
     /**
      * Is this point lexicographically smaller than that one? comparing
      * y-coordinates and breaking ties by x-coordinates
      */
     public int compareTo(Point that) {
-        // TODO: Implement this
-        return 0;
+        if ((this.y < that.y) || (this.x < that.x && this.y == that.y ))
+            return -1;
+        else if (this.y == that.y && this.x == that.x)
+            return 0;
+        else
+            return 1;
     }
+
 
     // return string representation of this point
     public String toString() {
